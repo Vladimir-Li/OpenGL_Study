62ed8eace0284ff19662d1ede901ef30bf1ef58c
Vladimir Li <vladimirrussia@foxmail.com>
2021-04-21 10:03:33 +0800

    MInor changes

--- OpenGL_Study/Camera.h
+++ OpenGL_Study/Camera.h
@@ -1,6 +1,20 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+#include <iostream>
+#include <string>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+class Camera {
+public:
+	Camera() {
+
+	}
+private:
+
+};
 
 #endif // !CAMERA_H
 
